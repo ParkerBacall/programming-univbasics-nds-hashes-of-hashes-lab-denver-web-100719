5eb6d319b162f55d9dbfc3dd61c0cd18d42734e4
Parker Bacall <pbacall2@gmail.com>
2019-10-20 22:12:24 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -37,7 +37,9 @@ def naming_system
   sub_category: {label: "Phylum",
   sub_category: {label: "Class",
   sub_category: {label: "Order"
-  }
+  sub_category: {label: "Family",
+  sub_category: {label: "Genus"
+      }
     }
   }
 }
